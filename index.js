b01f00071479d6b803387fd547fb7ce7c7c5191e
Shubh Agrawal <83549476+TheBassGuy@users.noreply.github.com>
2023-02-05 19:23:17 +0530

    Add to index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const guessInput = document.getElementById('guess-input');
+const guessButton = document.getElementById('guess-button');
+const result = document.getElementById('result');
+let targetNumber = Math.floor(Math.random() * 100) + 1;
 
+guessButton.addEventListener('click', function() {
+  const guess = parseInt(guessInput.value, 10);
+  if (guess === targetNumber) {
+    result.innerHTML = 'You win!';
+  } else if (guess < targetNumber) {
+    result.innerHTML = 'Too low';
+  } else {
+    result.innerHTML = 'Too high';
+  }
+});
